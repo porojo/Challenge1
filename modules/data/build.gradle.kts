17f02e3685c0e784ce7c64bc310e0d626355123e
Mambo <mambobryan@gmail.com>
2024-06-12 23:07:59 +0300

    :construction: chore/modularisation : updates data to depend on domain

--- modules/data/build.gradle.kts
+++ modules/data/build.gradle.kts
@@ -34,6 +34,8 @@ android {
 
 dependencies {
 
+    implementation(projects.modules.domain)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
