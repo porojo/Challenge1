7eecee88345099eebf57ad902f66f38f7a4c5b4a
Mambo <mambobryan@gmail.com>
2024-06-12 23:09:17 +0300

    :construction: chore/modularisation : updates app to depend on features and ui

--- modules/app/build.gradle.kts
+++ modules/app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 
 dependencies {
 
+    implementation(projects.modules.features)
+    implementation(projects.modules.ui)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
