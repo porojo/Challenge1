ddf2fedd4c552ac7b66f991ea51c6d671d32d44d
Mambo <mambobryan@gmail.com>
2024-06-12 11:11:49 +0300

    :construction: chore/modules : adds ui module to store all ui based components

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,6 +22,7 @@ dependencyResolutionManagement {
 
 rootProject.name = "MoneySwift"
 include(":modules")
+include(":modules:ui")
 include(":modules:app")
 include(":modules:data")
 include(":modules:domain")
