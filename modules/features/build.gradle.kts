a5c66cff03797ebcb8c4f606636ea65998bb8fca
jacqui <gitaujaquiline@gmail.com>
2024-06-27 10:04:43 +0300

    Adds ProductListItemComponent and ProductDetailsScreenComponent

--- modules/features/build.gradle.kts
+++ modules/features/build.gradle.kts
@@ -46,6 +46,10 @@ dependencies {
     //compose
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
 
     implementation(dependencyNotation = libs.kotlinx.coroutines)
 }
--- modules/features/src/main/kotlin/org/porojo/moneyswift/features/components/ProductListComponent.kt
+++
@@ -1,8 +0,0 @@
-package org.porojo.moneyswift.features.components
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun ProductListComponent(modifier: Modifier = Modifier) {
-}
