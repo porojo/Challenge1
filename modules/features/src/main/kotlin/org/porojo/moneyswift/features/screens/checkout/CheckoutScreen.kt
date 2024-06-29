13abf81a7c6b410b5f8258d0dda8425025ed778c
Mambo <mambobryan@gmail.com>
2024-06-29 21:59:30 +0300

    :construction: chore/checkout : updates CheckoutScreen to use checkout ViewModel

--- modules/features/src/main/kotlin/org/porojo/moneyswift/features/screens/checkout/CheckoutScreen.kt
+++ modules/features/src/main/kotlin/org/porojo/moneyswift/features/screens/checkout/CheckoutScreen.kt
@@ -1,5 +1,6 @@
 package org.porojo.moneyswift.features.screens.checkout
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,40 +15,39 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.koin.koinScreenModel
 import model.Product
 
 class CheckoutScreen(val product: Product) : Screen {
 
     @Composable
     override fun Content() {
-
-        // Get the product from the arguments: val product: Product
-
-
-        CheckoutScreenContent(
-            product = product
-        )
+        val screenModel: CheckoutScreenModel = koinScreenModel()
+        val state by screenModel.state.collectAsState()
+        screenModel.onValueChange(product = product)
+        CheckoutScreenContent(state = state)
     }
 
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CheckoutScreenContent(
-    product: Product // TODO : change this to view model
-) {
+fun CheckoutScreenContent(state: CheckoutScreenUiState) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         containerColor = MaterialTheme.colorScheme.background,
@@ -64,58 +64,76 @@ fun CheckoutScreenContent(
             )
         },
         content = { paddingValues ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                ElevatedCard(
-                    shape = RoundedCornerShape(size = 16.dp),
-                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
-                    colors = CardDefaults.elevatedCardColors(containerColor = MaterialTheme.colorScheme.surface)
-                ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceEvenly,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Image(
-                            painter = painterResource(product.productThumbnail),
-                            contentDescription = null,
-                            modifier = Modifier.size(100.dp)
-                        )
-                        Column {
-                            Text(
-                                text = product.productName,
-                                style = MaterialTheme.typography.titleMedium,
-                                fontWeight = FontWeight.Bold,
-                                color = MaterialTheme.colorScheme.onSurface
-                            )
+            AnimatedContent(targetState = state.product, label = "checkout animation") {
+                when (it) {
+                    null -> {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            CircularProgressIndicator()
+                        }
+                    }
 
-                            Text(
-                                text = "Kes: ${product.productPrice}",
-                                style = MaterialTheme.typography.titleLarge,
-                                fontWeight = FontWeight.Normal,
-                                color = MaterialTheme.colorScheme.onSurface
-                            )
+                    else -> {
+                        val product = it
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(paddingValues),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            ElevatedCard(
+                                shape = RoundedCornerShape(size = 16.dp),
+                                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                                colors = CardDefaults.elevatedCardColors(containerColor = MaterialTheme.colorScheme.surface)
+                            ) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceEvenly,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Image(
+                                        painter = painterResource(product.productThumbnail),
+                                        contentDescription = null,
+                                        modifier = Modifier.size(100.dp)
+                                    )
+                                    Column {
+                                        Text(
+                                            text = product.productName,
+                                            style = MaterialTheme.typography.titleMedium,
+                                            fontWeight = FontWeight.Bold,
+                                            color = MaterialTheme.colorScheme.onSurface
+                                        )
+
+                                        Text(
+                                            text = "Kes: ${product.productPrice}",
+                                            style = MaterialTheme.typography.titleLarge,
+                                            fontWeight = FontWeight.Normal,
+                                            color = MaterialTheme.colorScheme.onSurface
+                                        )
+                                    }
+                                }
+                            }
+
+                            Button(
+                                modifier = Modifier.fillMaxWidth(),
+                                onClick = {},
+                                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
+                            ) {
+                                Text(
+                                    text = "Pay Kes:${product.productPrice}",
+                                    style = MaterialTheme.typography.titleLarge,
+                                    fontWeight = FontWeight.Bold,
+                                    color = MaterialTheme.colorScheme.onPrimary
+                                )
+                            }
                         }
                     }
                 }
 
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = {},
-                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
-                ) {
-                    Text(
-                        text = "Pay Kes:${product.productPrice}",
-                        style = MaterialTheme.typography.titleLarge,
-                        fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colorScheme.onPrimary
-                    )
-                }
             }
 
         }
